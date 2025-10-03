dd4d2187077288ff324d058ac7feae9b15d524e4
SayamKhatri <khatri.say@northeastern.edu>
2025-10-03 14:00:54 -0400

    Added Model register implementation

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,6 +5,15 @@ from sagemaker.workflow.steps import ProcessingStep, TrainingStep
 from sagemaker.workflow.pipeline import Pipeline
 from sagemaker.inputs import TrainingInput
 import yaml, os
+from sagemaker.workflow.properties import PropertyFile
+from sagemaker.workflow.step_collections import RegisterModel
+from sagemaker.workflow.conditions import ConditionGreaterThanOrEqualTo
+from sagemaker.workflow.condition_step import ConditionStep
+from sagemaker.workflow.functions import JsonGet
+from sagemaker.model_metrics import MetricsSource, ModelMetrics
+from sagemaker.workflow.functions import Join
+
+
 
 CONFIG_PATH = os.path.join("config", "pipeline_config.yaml")
 with open(CONFIG_PATH, "r") as f:
@@ -89,10 +98,43 @@ step_train = TrainingStep(
     },
 )
 
+# Retrive metrics
+# Retrieve metrics
+model_metrics = ModelMetrics(
+    model_statistics=MetricsSource(
+        s3_uri=Join(
+            on="/",
+            values=[
+                step_train.properties.OutputDataConfig.S3OutputPath,
+                step_train.properties.TrainingJobName,
+                "output/data/evaluation_results.json"
+            ]
+        ),
+        content_type="application/json"
+    )
+)
+
+
+# Model registeration step
+step_register = RegisterModel(
+    name='NerModel',
+    estimator=huggingface_estimator,
+    model_data=step_train.properties.ModelArtifacts.S3ModelArtifacts,
+    image_uri=config["image_uri"],
+    content_types=["application/json"],
+    response_types=["application/json"],
+    inference_instances=["ml.m5.xlarge", "ml.m5.2xlarge"],
+    transform_instances=["ml.m5.xlarge", "ml.m5.2xlarge"],
+    model_package_group_name="biobert-ner-models",
+    approval_status="PendingManualApproval",
+    model_metrics=model_metrics,
+)
+
+
 # Pipeline
 pipeline = Pipeline(
     name="NERPreprocessAndTrainPipeline",
-    steps=[step_preprocess, step_train],
+    steps=[step_preprocess, step_train, step_register],
     sagemaker_session=sess,
 )
 
@@ -101,4 +143,3 @@ execution = pipeline.start()
 
 print(f"Pipeline started: {execution.arn}")
 
-#  MODEL REGISTRATION STEP 
